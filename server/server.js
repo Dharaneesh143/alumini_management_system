7ec334417722ca1ee81ec66a6bd716a65914ebd0
Dharaneesh143 <dharaneeshgl45@gmail.com>
2026-02-05 16:17:35 +0530

    next commit

--- server/server.js
+++ server/server.js
@@ -50,6 +50,7 @@ const PORT = process.env.PORT || 5000;
 const allowedOrigins = [
     'http://localhost:5173',
     'http://localhost:3000',
+    'https://aluminimanagementsystem.vercel.app',
     process.env.FRONTEND_URL // Useful for production
 ].filter(Boolean);
 
